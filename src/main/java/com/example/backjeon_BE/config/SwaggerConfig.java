35d7341cf83ce4a096114c35e69538f6f2fff988
doyulll <ajin7385@naver.com>
2026-02-01 00:29:06 +0900

    feat: Add Swagger API documentation

---
+++ src/main/java/com/example/backjeon_BE/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.example.backjeon_BE.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.Components;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("Backjeon API")
+                .version("1.0.0")
+                .description("장기 게임 백엔드 API 문서")
+                .contact(new Contact()
+                        .name("Backjeon Team")
+                        .email("your-email@example.com"));
+
+        // JWT 인증 설정
+        String jwtSchemeName = "JWT Token";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
